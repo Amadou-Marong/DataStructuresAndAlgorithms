3ad27195909aaeafe5c54b0ad1057509e0b8cc9d
Amadou-Marong <marongamadou6@gmail.com>
2024-09-09 11:03:58 +0100

    arrays in python

--- PythonDataStructures/arrays.py
+++ PythonDataStructures/arrays.py
@@ -4,3 +4,9 @@ arr_int = array.array('i', [1, 2, 3, 4, 5])
 print('Integer Array:', arr_int.tolist())
 
 # if you want to work with characters
+
+arr_char = list('hello')
+print('Character Array:', arr_char)
+
+print('Array Length:', len(arr_char))
+
