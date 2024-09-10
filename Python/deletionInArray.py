d241af57f60d2216894514157919524b0ef014e0
Amadou-Marong <marongamadou6@gmail.com>
2024-09-10 14:13:20 +0100

    defind delete function

--- Python/deletionInArray.py
+++ Python/deletionInArray.py
@@ -3,5 +3,16 @@ import array
 def findElement(arr, key):
     for i in range(len(arr)):
         if arr[i] == key:
+            # If the element is found, return the index
             return i
+    # If the element is not found, return -1
     return -1
+
+
+# Delete an element from an array
+def deleteElement(arr, key):
+    pos = findElement(arr, key)
+
+    if pos == -1:
+        print('Element not found in the array')
+
