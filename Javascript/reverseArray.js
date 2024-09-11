cc44a4ddb41e985f1e8c22f21e24436d2d603ed8
Amadou-Marong <marongamadou6@gmail.com>
2024-09-11 12:55:22 +0100

    reverse array in javascript

---
+++ Javascript/reverseArray.js
@@ -0,0 +1,30 @@
+// Iterative program to reverse an array
+
+// function to reverse arr[] from start to end
+
+function reverseArray(arr, start, end){
+    while (start < end){
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start ++
+        end --
+    }
+}
+
+function printArray(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6]
+
+reverseArray(arr, 0, arr.length-1)
+
+console.log(arr);
+
+
+
+
+
