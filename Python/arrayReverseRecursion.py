5f104fe52a1bbc8559c098b3d860e624ea55999e
Amadou-Marong <marongamadou6@gmail.com>
2024-09-11 13:56:22 +0100

    Recursive python program to reverse an array

---
+++ Python/arrayReverseRecursion.py
@@ -0,0 +1,9 @@
+# recursive python program to reverse an array
+
+#  function to reverse A[] from start to end
+
+def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    
