0d8e7d7d24b9e4a952f03a3349eb079abf55ad5a
Amadou-Marong <marongamadou6@gmail.com>
2024-09-11 16:28:43 +0100

    Recursive python program to reverse an array

--- Python/arrayReverseRecursion.py
+++ Python/arrayReverseRecursion.py
@@ -8,5 +8,13 @@ def reverseList(A, start, end):
     A[start], A[end] = A[end], A[start]
     reverseList(A, start+1, end-1)
 
+# Driver function to test the above function
 
+A = [1, 2, 3, 4, 5, 6]
 
+print(A)
+
+reverseList(A, 0, 5)
+
+print("Reversed List is")
+print(A)
