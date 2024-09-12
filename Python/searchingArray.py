0215f1bab58947687d3a43d7bcc22b46d0d6b534
Amadou-Marong <marongamadou6@gmail.com>
2024-09-12 16:29:36 +0100

    example usage of find element in array

--- Python/searchingArray.py
+++ Python/searchingArray.py
@@ -3,4 +3,13 @@ def findElement(arr, n, key):
         if arr[i] == key:
             return i
         return -1
+
+
+# Example usage
+if __name__ == "__main__":
+    # Define an unsorted array
+    arr = [4, 2, 5, 3, 8, 6, 5, 9]
+
+    # key to search for
+    key = 9
     
