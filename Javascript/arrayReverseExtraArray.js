a2de4d118cb77e73e143df907675ef1aebf64377
Amadou-Marong <marongamadou6@gmail.com>
2024-09-17 02:15:11 +0100

    reverse array extra array javascript

--- Javascript/arrayReverseExtraArray.js
+++ Javascript/arrayReverseExtraArray.js
@@ -1,3 +1,9 @@
 function reverseArrayExtraArray(arr) {
-    const reversedArray = arr.slice().reverse()
+    const reversedArr = arr.slice().reverse()
+
+    // print reversed array
+    process.stdout.write("Reversed Array: ")
+    reversedArr.foreach(element => process.stdout.write(element + " "));
 }
+
+// Example usage
