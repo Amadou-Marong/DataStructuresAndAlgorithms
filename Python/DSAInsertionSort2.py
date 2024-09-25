42d12051f7684fc955015319843d7f29522e6647
Amadou-Marong <marongamadou6@gmail.com>
2024-09-25 12:01:03 +0100

    DSA Insertion sort

--- Python/DSAInsertionSort2.py
+++ Python/DSAInsertionSort2.py
@@ -4,6 +4,17 @@ n = len(my_array)
 
 for i in range(1, n):
     insert_index = i
-    current_valiue = my_array[i]
+    current_value = my_array[i]
 
-    for j in range()
+    for j in range(i-1, -1, -1):
+        if my_array[j] > current_value:
+            my_array[j+1] = my_array[j]
+            insert_index = j
+        else:
+            break
+    
+    my_array[insert_index] = current_value
+
+
+print("Sorted Array", my_array)
+
