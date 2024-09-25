7524168e45401eeb4f1bebbed708d93fcfc8f92d
Amadou-Marong <marongamadou6@gmail.com>
2024-09-25 01:26:23 +0100

    DSA Bubble sort

--- Python/DSAInsertionSort.py
+++ Python/DSAInsertionSort.py
@@ -8,3 +8,10 @@ for i in range(1, n):
     
     for j in range(i-1, -1, -1):
         if my_array[j] > current_value:
+            insert_index = j
+    
+    my_array.insert(insert_index, current_value)
+
+
+print("Sorted Array", my_array)
+
