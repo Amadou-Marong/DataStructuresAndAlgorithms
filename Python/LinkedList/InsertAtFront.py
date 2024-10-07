6034e495746faec455ed067feae631840e6dd857
Amadou-Marong <marongamadou6@gmail.com>
2024-10-07 16:22:25 +0100

    insert at front

--- Python/LinkedList/InsertAtFront.py
+++ Python/LinkedList/InsertAtFront.py
@@ -1,8 +1,8 @@
 # python program to insert a node at the front of the linked list
 
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, new_data):
+        self.data = new_data
         self.next = None
 
 
@@ -15,3 +15,25 @@ def insert_at_front(head, new_data):
 
     # return the new node as the new head of the list
     return new_node
+
+
+def print_list(head):
+    curr = head
+    while curr is not None:
+        print(f"{curr.data}", end="->")
+        curr = curr.next
+    print()
+
+# code execution starts here
+if __name__ == "__main__":
+
+    # create a linked list 2->3->4->5
+    head = Node(2)
+    head.next = Node(3)
+    head.next.next = Node(4)
+    head.next.next.next = Node(5)
+
+    data = 1
+
+    head = insert_at_front(head, data)
+    print_list(head)
