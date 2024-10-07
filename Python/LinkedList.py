c600311734270df983a5655902b8a9e51bb46850
Amadou-Marong <marongamadou6@gmail.com>
2024-10-07 14:10:43 +0100

    linked list

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -1,6 +1,14 @@
+# Node class
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+# Linked List class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
 
 
