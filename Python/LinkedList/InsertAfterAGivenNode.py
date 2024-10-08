d405f9ee5ffbd8b4d47e9c8ff58668d997f4bca9
Amadou-Marong <marongamadou6@gmail.com>
2024-10-08 17:00:29 +0100

    fuction to printlist

--- Python/LinkedList/InsertAfterAGivenNode.py
+++ Python/LinkedList/InsertAfterAGivenNode.py
@@ -3,3 +3,11 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+# create a function to print the nodes
+def print_list(head):
+    curr = head
+    while curr is not None:
+        print(f"{curr.data}", end="->")
+        curr = curr.next
+
