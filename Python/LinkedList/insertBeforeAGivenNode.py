93aeaf60e7848b583472fec89f0ff9878fb6ac09
Amadou-Marong <marongamadou6@gmail.com>
2024-10-09 14:20:58 +0100

    fuction to insert before a given node

---
+++ Python/LinkedList/insertBeforeAGivenNode.py
@@ -0,0 +1,16 @@
+# create a node class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# create a function to insert a node before a given node
+
+def insert_before(head, key, new_data):
+    pass
+
+# create a function to print the nodes
+
+def print_list(head):
+    pass
+
