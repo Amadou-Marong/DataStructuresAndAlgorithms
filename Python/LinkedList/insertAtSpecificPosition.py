3e34803f25e7fff1f99ef632c3047c3d09babc53
Amadou-Marong <marongamadou6@gmail.com>
2024-10-11 11:39:21 +0100

    insert a specific position in a linked list

--- Python/LinkedList/insertAtSpecificPosition.py
+++ Python/LinkedList/insertAtSpecificPosition.py
@@ -6,3 +6,24 @@ class Node:
         self.next = None
 
 
+# function to insert a node at a specific position in linked list
+def insert_at(head, postion):
+    pass
+
+
+# function to print linked list
+def print_list(head):
+    curr = head
+    while curr is not None:
+        print(f"{curr.data}", end="->")
+        curr = curr.next
+    print()
+
+# code execution starts here
+if __name__ == "__main__":
+    head = Node(3)
+    head.next = Node(5)
+    head.next.next = Node(8)
+    head.next.next.next = Node(10)
+
+    print_list(head)
