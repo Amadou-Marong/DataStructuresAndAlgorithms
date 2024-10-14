d596d8e998b0b884c26cfa541facbcc586bdf63d
Amadou-Marong <marongamadou6@gmail.com>
2024-10-14 16:33:50 +0100

    reverse a linked list iterative method

---
+++ Python/LinkedList/ReverseLinkedListIterative.py
@@ -0,0 +1,24 @@
+# iterative method to reverse a linked list 
+
+# create a node class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# function to printList
+def print_list(head):
+    curr = head
+    while curr is not None:
+        print(f"{curr.data}", end="->")
+        curr = curr.next
+    print()
+
+
+# code execution starts here 
+if __name__ == "__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
