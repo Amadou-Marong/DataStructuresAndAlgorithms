0b43f8d998003096c166ab80148cfe6ba577f0df
Amadou-Marong <marongamadou6@gmail.com>
2024-10-14 17:12:20 +0100

    reverse a linked list iterative method

--- Python/LinkedList/ReverseLinkedListIterative.py
+++ Python/LinkedList/ReverseLinkedListIterative.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
     head.next = Node(2)
     head.next.next = Node(3)
     head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
 
     
     print_list(head)
