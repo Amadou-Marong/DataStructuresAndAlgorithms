12091b8ebc851c2937585ef0f0b9de714b6f9146
Amadou-Marong <marongamadou6@gmail.com>
2024-10-15 14:07:43 +0100

    check if given expression is valid parenthesis

--- Python/Stack/ValidParenthesis.py
+++ Python/Stack/ValidParenthesis.py
@@ -1,5 +1,18 @@
 # define a function to check for valid parenthesis
 
 def is_valid_parenthesis(s):
-    pass
+    stack = []
+
+    for char in s:
+
+        # opening the bracket
+        if char in '({[':
+            stack.append(char)
+        
+        # closing the bracket
+
+        if char in ')}]':
+            pass
+
+
 
