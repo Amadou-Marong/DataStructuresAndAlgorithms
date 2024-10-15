71cdd7523ebe4df219de5c478ca82cf83b52f022
Amadou-Marong <marongamadou6@gmail.com>
2024-10-15 15:44:32 +0100

    check if given expression is valid parenthesis

--- Javascript/Stack/ValidParenthesis.js
+++ Javascript/Stack/ValidParenthesis.js
@@ -3,8 +3,23 @@ const isValidParenthesis = (s) => {
 
     for (let i=0; i<stack.length; i++) {
         
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-            
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(stack[i])
+        } else {
+            // if its's a closing bracket, check if the stack is non-empty
+            // and if the top of the stack is a matching opening bracket
+            if (stack.length > 0 && 
+                ((stack[stack.length -1] === '(' && s[i] === ')')) || 
+                ((stack[stack.length -1] === '{' && s[i] === '}')) || 
+                ((stack[stack.length -1] === ']' && s[i] === ']'))
+            ) {
+                stack.pop() // Pop the matching opening bracket
+            } else {
+                return false  // Unmatched closing bracket
+            }
         }
     }
+
+    // If stack is empty, return true (balanced), otherwise false
+    return stack.length === 0;
 }
