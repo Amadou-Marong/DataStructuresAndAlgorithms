2d276621569ad53a80ec4129ccc01cef5b808d48
Amadou-Marong <marongamadou6@gmail.com>
2024-10-15 13:32:42 +0100

    stack example

--- Python/Stack/Example.py
+++ Python/Stack/Example.py
@@ -1,7 +1,18 @@
+# stack
 stack = []
+
+# push elements to stack
 stack.append("a")
 stack.append("b")
 stack.append("c")
 
 print("initial stack")
 print(stack)
+
+# pop elements from stack
+stack.pop()
+stack.pop()
+stack.pop()
+print("Stack after poping elements")
+print(stack)
+
