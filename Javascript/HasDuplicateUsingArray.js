d4a19907cd3aab5415098efa2dbfdc1f8820eb98
Amadou-Marong <marongamadou6@gmail.com>
2024-10-16 13:15:07 +0100

    check if given array has duplicate values

--- Javascript/HasDuplicateUsingArray.js
+++ Javascript/HasDuplicateUsingArray.js
@@ -1,6 +1,5 @@
 function hasDuplicateValue(array){
     let existingNumbers = []
-    
     for (let i=0; i<array.length; i++){
         if (existingNumbers[array[i]] === undefined) {
             existingNumbers[array[i]] = 1;
@@ -9,7 +8,6 @@ function hasDuplicateValue(array){
         }
     }
 
-    console.log(existingNumbers);
     return false
 }
 array = [3, 5, 6, 3]
