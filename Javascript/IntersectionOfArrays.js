982c30fdf88ef786f6af6916744162dc3b989453
Amadou-Marong <marongamadou6@gmail.com>
2024-10-18 13:34:52 +0100

    intersection of two arrays

--- Javascript/IntersectionOfArrays.js
+++ Javascript/IntersectionOfArrays.js
@@ -1,1 +1,20 @@
 // This program is to check the intersection of two arrays
+
+function instersection(first_array, second_array) {
+    let result = []
+
+    for (let i=0; i<first_array.length; i++) {
+
+        for (let j=0; j<second_array.length; i++) {
+            if(first_array[i] === second_array[j]) {
+                result.push(first_array[i])
+            }    
+        }
+    }
+    return result
+}
+
+let first_array = [3, 1, 4, 2]
+let second_array = [4, 5, 3, 6]
+
+console.log(instersection(first_array, second_array));
