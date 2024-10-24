1c2c9b225efb46edabd0fd4ea04bb82d946ba768
Amadou-Marong <marongamadou6@gmail.com>
2024-10-24 14:41:04 +0100

    priority queue

--- Python/Queue/PriorityQueue.py
+++ Python/Queue/PriorityQueue.py
@@ -9,5 +9,35 @@ class PriorityQueue(object):
     
     def is_empty(self):
         return len(self.queue) == 0
+
+    def join_queue(self, data):
+        self.queue.append(data)    
     
+    def serve(self):    
+            try:
+                max = 0
+                for i in range(len(self.queue)):
+                    if self.queue[i] > self.queue[max]:
+                        max = i
+                
+                item = self.queue[max]
+                del self.queue[max]
+                return item
     
+            except IndexError:
+                print()
+                exit()
+
+
+queue = PriorityQueue()
+queue.join_queue(12)
+queue.join_queue(1)
+queue.join_queue(14)
+queue.join_queue(7)
+
+print(queue)
+
+while not queue.is_empty():
+    print(f"{queue.serve()}", end=", ")
+
+        
