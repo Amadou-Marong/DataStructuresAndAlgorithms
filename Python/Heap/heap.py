e2410122a6457ca479b1cd7c7a5874aa5bb2e09d
Amadou-Marong <marongamadou6@gmail.com>
2024-10-25 12:34:06 +0100

    heap queue

---
+++ Python/Heap/heap.py
@@ -0,0 +1,14 @@
+# implementation of a heap
+
+# importing the heapq to implement the heap queue
+import heapq
+
+my_list = [5, 7, 9, 1, 3]
+
+# using the heapify method to convert the list to heap
+print(my_list)
+heapq.heapify(my_list)
+
+# print the heap queue
+
+print(my_list)
