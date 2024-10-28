37887984a5b060cb4b1f5fc1c2d6acac7853d96d
Amadou-Marong <marongamadou6@gmail.com>
2024-10-28 15:29:02 +0000

    node class in a binary tree

---
+++ Python/BinaryTree/BinaryTree.py
@@ -0,0 +1,7 @@
+# a python class that represents an individual node in a binary tree
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
