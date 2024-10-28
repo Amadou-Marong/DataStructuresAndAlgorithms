396118fa3bd0d04ded0f4a7c5f9c694c301fff03
Amadou-Marong <marongamadou6@gmail.com>
2024-10-28 15:34:45 +0000

    node class in a binary tree

--- Python/BinaryTree/BinaryTree.py
+++ Python/BinaryTree/BinaryTree.py
@@ -15,11 +15,12 @@ root = Node(1)
     None None'''
 
 root.left = Node(2)
+
+root.right = Node(3)
+
 ''' 2 and 3 become left and right children of 1
         1
         / \
         2     3
     / \ / \
 None None None None'''
-
-root.right = Node(3)
