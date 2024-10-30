1badf65638d62e3a13cc9bc7ecd9a5cc472595a1
Amadou-Marong <marongamadou6@gmail.com>
2024-10-30 11:26:03 +0000

    inorder traversal of a binary tree

--- Python/BinaryTree/InorderTraversal.py
+++ Python/BinaryTree/InorderTraversal.py
@@ -6,3 +6,26 @@ class Node:
         self.left = None
         self.right = None
 
+
+def inorderTraversal(root):
+
+    # base case if root is none
+    if root is None:
+        return
+    
+    # recur on the left subtree
+    inorderTraversal(root.left)
+    
+    # visit the current node
+    print(root.data, end=" ")
+
+    # recur on the right subtree
+    inorderTraversal(root.right)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+inorderTraversal(root)
