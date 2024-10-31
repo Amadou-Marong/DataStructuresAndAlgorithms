407f779c0ebf38244e8841774bd4e714c5b2a94f
Amadou-Marong <marongamadou6@gmail.com>
2024-10-31 15:23:56 +0000

    levelorder traversal of a binary tree

---
+++ Python/BinaryTree/LevelorderTraversal.py
@@ -0,0 +1,8 @@
+# python program for levelorder traversal of a binary tree
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
