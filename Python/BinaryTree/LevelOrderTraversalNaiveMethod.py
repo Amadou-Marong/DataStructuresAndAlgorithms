7f29797f75fd4f68277763fa07544a9db69510fc
Amadou-Marong <marongamadou6@gmail.com>
2024-11-01 11:44:43 +0000

    levelorder traversal of a binary tree naive method

---
+++ Python/BinaryTree/LevelOrderTraversalNaiveMethod.py
@@ -0,0 +1,9 @@
+# Recursive program for level order ttraversal of Binary Tree
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    
