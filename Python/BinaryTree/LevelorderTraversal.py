509e51b26e3e323b3291d17194670bc2effa6f0a
Amadou-Marong <marongamadou6@gmail.com>
2024-11-01 10:48:55 +0000

    levelorder traversal of a binary tree

--- Python/BinaryTree/LevelorderTraversal.py
+++ Python/BinaryTree/LevelorderTraversal.py
@@ -18,3 +18,21 @@ def printLevelOrder(root):
     # create an empty queue for level order traversal
     queue = []
 
+    # Enqueue Root and initialize height
+    queue.append(root)
+
+    while(len(queue) > 0):
+        # Print front of queue and
+        # remove it from queue
+        print(queue[0].data)
+
+        node = queue.pop(0)
+
+        # Enqueue left child
+        if node.left is not None:
+            queue.append(node.left)
+
+        # Enqueue right child
+        
+
+    
