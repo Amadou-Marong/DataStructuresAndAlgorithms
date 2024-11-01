ce3d28aa279e494551be9d7b39045d5aafb1d00f
Amadou-Marong <marongamadou6@gmail.com>
2024-11-01 12:27:28 +0000

    levelorder traversal of a binary tree naive method

--- Python/BinaryTree/LevelOrderTraversalNaiveMethod.py
+++ Python/BinaryTree/LevelOrderTraversalNaiveMethod.py
@@ -6,4 +6,21 @@ class Node:
         self.left = None
         self.right = None
 
+# function to print level order traversal of a tree
+
+
+
+
+# compute the height of a tree
+def height(node):
+    if node is None:
+        return 0
+    else:
+        lheight = height(node.left)
+        rheight = height(node.right)
+        if lheight > rheight:
+            return lheight + 1
+        else:
+            return rheight + 1
+    
     
