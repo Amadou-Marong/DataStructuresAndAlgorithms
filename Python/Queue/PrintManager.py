ab3d6d809eb671ef0514469c6ccad06765c062da
Amadou-Marong <marongamadou6@gmail.com>
2024-11-04 18:43:32 +0000

    Queue for Print Manager

---
+++ Python/Queue/PrintManager.py
@@ -0,0 +1,25 @@
+class PrintManager:
+    def __init__(self):
+        self.queue = []
+
+
+    def queue_print_job(self, document):
+        self.queue.append(document)
+    
+    def run(self):
+        while (len(self.queue) != 0):
+            print(self.queue.pop(0))
+        
+        
+
+printManager = PrintManager()
+
+ducument1 = "First Document"
+ducument2 = "Second Document"
+ducument3 = "Third Document"
+
+printManager.queue_print_job(ducument1)
+printManager.queue_print_job(ducument2)
+printManager.queue_print_job(ducument3)
+
+printManager.run()
