58438210e5b6693c00f2417a7e6f42fd09b043fc
Amadou-Marong <marongamadou6@gmail.com>
2024-11-05 14:31:35 +0000

    Search a key in BST

---
+++ Python/BinarySearchTree/SearchingElement.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+# fuction to searc a key in a BST
+def search(root, key):
+
+    
