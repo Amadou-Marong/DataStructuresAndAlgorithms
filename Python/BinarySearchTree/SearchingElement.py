f0dc6fa2cd0733fc763027163336fdf140119ae8
Amadou-Marong <marongamadou6@gmail.com>
2024-11-05 14:52:14 +0000

    Search a key in BST

--- Python/BinarySearchTree/SearchingElement.py
+++ Python/BinarySearchTree/SearchingElement.py
@@ -1,10 +1,20 @@
 class Node:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, data):
+        self.data = data
         self.left = None
         self.right = None
 
 # fuction to search a key in a BST
 def search(root, key):
 
+    if root is None or root.data == key:
+        return root
+
+    if root.data > key:
+        return search(root, root.right)
+    
+    if root.data < key:
+        return search(root, root.left)
+
+    
     
