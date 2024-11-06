6be53f42f7c89525757e7fa43c651e800ee990bc
Amadou-Marong <marongamadou6@gmail.com>
2024-11-06 11:40:12 +0000

    Search a key in BST

---
+++ Javascript/BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+}
+
+function search(root, key) {
+    if (root == null || root.data == key) {
+        return root
+    }
+    if (root.data < key) {
+        return search(root.right, key)
+    }
+
+    return search(root.left, key)
+}
+
+let root = new Node(50)
+root.left = new Node(30)
+root.right = new Node(70)
+root.left.left = new Node(20)
+root.left.right = new Node(40)
+root.right.left = new Node(60)
+root.right.right = new Node(80)
+
+console.log(search(root, 10) !== null ? "Found" : "Not Found");
+
+
+
+// root = Node(50)
+// root.left = Node(30)
+// root.right = Node(70)
+// root.left.left = Node(20)
+// root.left.right = Node(40)
+// root.right.left = Node(60)
+// root.right.right = Node(80)
