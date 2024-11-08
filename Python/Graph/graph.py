9549d1afe1c29ec0b48ae0e52343f4254c78be03
Amadou-Marong <marongamadou6@gmail.com>
2024-11-08 19:58:54 +0000

    Graph

--- Python/Graph/graph.py
+++ Python/Graph/graph.py
@@ -1,1 +1,8 @@
 # a simple representation of graph using Adjancency matrix
+
+class Graph:
+    def __init__(self, numvertex):
+        self.adjMatrix = [[-1]*numvertex for x in range(numvertex)]
+        self.numvertex = numvertex
+        self.vertices = {}
+        self.verticeslist = [0]*numvertex
