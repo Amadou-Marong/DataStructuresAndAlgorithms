127d0b0d1633e1f433f7cd7be8d6a23d631489b7
Amadou-Marong <marongamadou6@gmail.com>
2024-11-14 12:33:51 +0000

    Binary search

---
+++ Python/SearchingAlgorithms/BinarySearch.py
@@ -0,0 +1,29 @@
+# program to implement binary search in python
+
+def binarySearch(arr, key):
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+    
+        if arr[mid] == key:
+            return mid
+        
+        if arr[left] == key:
+            return left
+        else:
+            return right
+        
+    return - 1
+         
+
+arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+key = 11
+
+result = binarySearch(arr, key)
+
+if result != -1:
+    print(f"The value {key} is found at index {result} of the array")
+else:
+    print(f"The value {key} is not found in the array")
