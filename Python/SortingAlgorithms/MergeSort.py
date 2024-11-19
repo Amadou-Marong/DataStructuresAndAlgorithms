c451682dc0d856aa5d35c9f8c2c5afb7e879dd72
Amadou-Marong <marongamadou6@gmail.com>
2024-11-19 15:50:58 +0000

    mergesort algorithm

--- Python/SortingAlgorithms/MergeSort.py
+++ Python/SortingAlgorithms/MergeSort.py
@@ -7,5 +7,10 @@ def merge(arr, left, mid, right):
     L = [0] * n1
     R = [0] * n2
     
+    # copy data to temp arrays
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[right + mid + j]
     
     
