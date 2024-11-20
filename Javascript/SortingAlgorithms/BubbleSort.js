0dbf06d0542f4127ccfd937c50342806c3d85c27
Amadou-Marong <marongamadou6@gmail.com>
2024-11-20 15:02:06 +0000

    bubblesort algorithm

---
+++ Javascript/SortingAlgorithms/BubbleSort.js
@@ -0,0 +1,34 @@
+// javascript program to implement bubblesort
+
+function bubbleSort(arr) {
+    let n = arr.length
+
+    for (let i=0; i < n; i++) {
+        let swapped = false
+        
+        for (let j=0; j<n-i-1; j++) {
+
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                swapped = true
+            }
+        }
+        if (swapped == true) break   
+    }
+}
+
+function printArray(arr) {
+    // let n = arr.length
+
+    for (val of arr) {
+        process.stdout.write(val + " ")
+    }
+}
+
+let arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+printArray(arr)
