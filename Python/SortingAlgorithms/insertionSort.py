375b72890446b8c359f84ca7685d41558c26ca20
Amadou-Marong <marongamadou6@gmail.com>
2024-11-20 13:46:30 +0000

    insertionsort algorithm

--- Python/SortingAlgorithms/insertionSort.py
+++ Python/SortingAlgorithms/insertionSort.py
@@ -1,24 +1,28 @@
 # python program to implement insertionsort algorithm
 
 def insertionSort(arr):
-    n = len(arr) - 1
+    n = len(arr)
     for j in range(1, n):
         key = arr[j]
         i = j - 1
         
-        while key >= 0 and key < arr[i]:
-            key = arr[i]
+        while i >= 0 and key < arr[i]:
+            arr[i + 1] = arr[i]
             i -= 1
         arr[i + 1] = key
 
 
 # function to print array elements
 def printArray(arr):
-    for val in arr:
-        print(val, end=" ")
+    n = len(arr)
+    for i in range(n):
+        print(arr[i], end=" ")
     print()
 
 
 if __name__ == "__main__":
     arr = [12, 11, 13, 5, 6]
-            
+    
+    insertionSort(arr)
+    
+    printArray(arr)
