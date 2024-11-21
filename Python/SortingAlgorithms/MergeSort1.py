1aadd5290c3eb0d883fece47056cb892ff214c2d
Amadou-Marong <marongamadou6@gmail.com>
2024-11-21 15:52:32 +0000

    mergesort algorithm

--- Python/SortingAlgorithms/MergeSort1.py
+++ Python/SortingAlgorithms/MergeSort1.py
@@ -18,3 +18,31 @@ def mergeSort(arr):
         # sort the second half
         mergeSort(R)
         
+        i = j = k = 0
+        
+        # copy the data to temp array L[], and R[]
+        while i <= len(L) and j <= len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+        k += 1
+        
+        # copy the remaining data
+        while i <= len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+            
+        while j <= len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+        
+
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]        
+        
