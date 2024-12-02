e44c369f561ee88492616da41d1d4c4f9840294d
Amadou-Marong <marongamadou6@gmail.com>
2024-12-02 02:16:50 +0000

    naive approach to find the nth fibbonacci number

--- Python/DynamicProgramming/NthFibbonacci.py
+++ Python/DynamicProgramming/NthFibbonacci.py
@@ -1,3 +1,12 @@
 def nth_fibonacci(n):
+    # Base case if n = 0 or n = 1
     if n <= 1:
-        
+        return n
+    
+    # Recursive case if the number is greater than 1
+    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2)
+
+
+if __name__ =="__main__":
+    n = 10
+    print(nth_fibonacci(n))
