0486150544ec583afeeb0241377032342f06f120
Amadou-Marong <marongamadou6@gmail.com>
2024-12-03 11:28:14 +0000

    memoization approach to find the nth fibbonacci number

--- Python/DynamicProgramming/MemoizationApproach.py
+++ Python/DynamicProgramming/MemoizationApproach.py
@@ -5,4 +5,9 @@ def nth_fibbonacci_util(n, memo):
     if n <= 1:
         return n
     
+      
+    if memo[n] != -1:
+        return memo[n]
+    
+    memo = nth_fibbonacci_util(n - 1, memo) + nth_fibbonacci_util(n - 2, memo)
     
