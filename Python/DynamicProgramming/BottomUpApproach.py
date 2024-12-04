0873094f41d4556da12db2e284107446a53cfa74
Amadou-Marong <marongamadou6@gmail.com>
2024-12-04 15:59:59 +0000

    buttom up approach to find the nth fibbonacci number

--- Python/DynamicProgramming/BottomUpApproach.py
+++ Python/DynamicProgramming/BottomUpApproach.py
@@ -13,3 +13,7 @@ def nth_fibbonacci(n):
     # use the bottom up approach
     for i in range(2, n + 1):
         dp[i] = dp[i - 1] + dp[i - 2]
+    
+    # we return the value at dp[n] which is the nth fibbonacci number 
+    return dp[n]
+
