f53a401828b06684ca53f9390490198f384dd6ca
Amadou-Marong <marongamadou6@gmail.com>
2024-12-24 14:37:55 +0000

    python program to print distict elements from an array

--- Python/ArrayProblems/Easy/DistinctElements.py
+++ Python/ArrayProblems/Easy/DistinctElements.py
@@ -2,4 +2,24 @@
 
 def findDistinct(arr):
     
+    results = []
+    
+    for i in range(len(arr)):
+        j = 0
+        
+        while(j < i):
+            # check if element is included in the results
+            if arr[i] == arr[j]:
+                break
+            j+=1
+
+        # add the element if not included previously
+        if j == i:
+            results.append(arr[i])
+            
+    return results
+
+if __name__ == "__main__":
+    arr = [12, 10, 9, 45, 2, 10, 10, 45]
+    print(findDistinct(arr))
     
