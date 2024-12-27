00514d9eae74aa475087e551adb7fa516aa1d963
Amadou-Marong <marongamadou6@gmail.com>
2024-12-27 22:35:00 +0000

    contains duplicate within k distance

--- Python/ArrayProblems/Easy/CheckDuplicatesWithinK.py
+++ Python/ArrayProblems/Easy/CheckDuplicatesWithinK.py
@@ -6,20 +6,22 @@ def containsDuplicatesWithinK(arr, k):
     for i in range(n):
         
         # loop through 1 to k
-        for j in range(i, k+1):
+        for c in range(1, k + 1):
+            
+            j = i + c
             
             # check if element occures more than once within the distance
-            if arr[i] == arr[j]:
-                return False
+            if j < n and arr[i] == arr[j]:
+                return True
             
-        return False
+    return False
     
 # driver program 
 # arr = [10, 5, 3, 4, 3, 5, 6]
 
-# arr = [1, 2, 3, 4, 1, 2, 3, 4]
+arr = [1, 2, 3, 4, 1, 2, 3, 4]
 
-arr = [1, 2, 3, 1, 4, 5]
+# arr = [1, 2, 3, 1, 4, 5]
 k = 3
 
-print("Yes" if containsDuplicatesWithinK(arr, k) else "No")
+print("True" if containsDuplicatesWithinK(arr, k) else "False")
