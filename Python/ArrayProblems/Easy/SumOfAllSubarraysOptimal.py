a96c203ea5b3a896c6a2c8adf276431689bbeb01
Amadou-Marong <marongamadou6@gmail.com>
2024-12-29 17:48:47 +0000

    optimal approach to find the sum of all subarrays of a given array

--- Python/ArrayProblems/Easy/SumOfAllSubarraysOptimal.py
+++ Python/ArrayProblems/Easy/SumOfAllSubarraysOptimal.py
@@ -6,16 +6,18 @@ def sub_array_sum(arr):
     
     # pick the starting point
     for i in range(n):
-        
+        temp = 0
         # pick the ending point
         for j in range(i, n):
             
             # sum the starting and ending points
-            result  += arr[j]
-    
+            temp  += arr[j]
+            result += temp
     return result
 
 
+arr = [1, 2, 3]
 
+print(sub_array_sum(arr))
 
 # optimal approach to find the sum of all subarrays of a given array
