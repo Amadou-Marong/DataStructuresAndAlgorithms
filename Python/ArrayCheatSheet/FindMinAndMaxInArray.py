d432eee12cad25e29a052d4403b1251f0d633047
Amadou-Marong <marongamadou6@gmail.com>
2025-01-01 18:30:18 +0000

    find the maximum from an array

--- Python/ArrayCheatSheet/FindMinAndMaxInArray.py
+++ Python/ArrayCheatSheet/FindMinAndMaxInArray.py
@@ -9,9 +9,19 @@ def set_min(arr):
             mini = num
     return mini
 
+def set_max(arr):
+    maxi = float('-inf')
+    
+    for num in arr:
+        if num > maxi:
+            maxi = num
+    return maxi
+
+
 if __name__ == "__main__":
 
     arr = [4, 9, 6, 5, 2, 3]
     
     print(set_min(arr))
+    print(set_max(arr))
     
