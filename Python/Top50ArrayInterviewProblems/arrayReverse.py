7b81fb174921f9a53085d06e9f21f4fd469b8505
Amadou-Marong <marongamadou6@gmail.com>
2025-01-02 11:51:47 +0000

    reverse an array

---
+++ Python/Top50ArrayInterviewProblems/arrayReverse.py
@@ -0,0 +1,14 @@
+# python program to reverse an array
+
+def reverse_array(arr):
+    n = len(arr)
+    
+    temp = [0] * n
+    
+    
+    return temp
+
+
+arr = [1, 4, 3, 2, 6, 5]    
+
+print(reverse_array(arr))
