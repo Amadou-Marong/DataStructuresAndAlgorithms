fab8e3bb291230b1a9e987faa85c69268b36ca4e
Amadou-Marong <marongamadou6@gmail.com>
2025-01-02 14:56:51 +0000

    reverse an array using two pointers approach

--- Python/Top50ArrayInterviewProblems/arrayReverse.py
+++ Python/Top50ArrayInterviewProblems/arrayReverse.py
@@ -1,5 +1,7 @@
 # python program to reverse an array
 
+# naive approach to reverse an array time O(n) space O(n)
+"""
 def reverse_array(arr):
     n = len(arr)
     
@@ -22,3 +24,28 @@ print(f"Original array: {arr}")
 reverse_array(arr)
 
 print(f"Reversed array: {arr}")
+
+"""
+
+# two pointers method
+
+def reverse_array(arr):
+    n = len(arr)
+    
+    right = 0
+    left = n
+    
+    while right < left:
+        
+        for i in range(n):
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            
+        right += 1
+        
+        left -= 1
+
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5] 
+    
+    print(f"Original array: {arr}")
