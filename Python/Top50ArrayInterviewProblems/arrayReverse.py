13e1689c7c32ee46f41d2900fbeef861f8591bf6
Amadou-Marong <marongamadou6@gmail.com>
2025-01-02 14:37:59 +0000

    reverse an array

--- Python/Top50ArrayInterviewProblems/arrayReverse.py
+++ Python/Top50ArrayInterviewProblems/arrayReverse.py
@@ -3,15 +3,13 @@
 def reverse_array(arr):
     n = len(arr)
     
+    # create a temporary array of length n and initial value 0
     temp = [0] * n
     
+    # store the values of the original array in reverse order in temp array
     for i in range(n):
-        temp[i] = arr[n-i-1]
-    
-    for i in range(n):
-        arr[i] = temp[i]
-        
-    return arr
+        temp[i] = arr[-i]
+
 
 arr = [1, 4, 3, 2, 6, 5]    
 
