7b54e38f12513d90d08c8eabb73c3def9ff5d1f1
Amadou-Marong <marongamadou6@gmail.com>
2025-01-04 21:37:46 +0000

    Second largest distinct element in an array

---
+++ Python/Top50ArrayInterviewProblems/SecondLargestInArray.py
@@ -0,0 +1,7 @@
+# python program to find the second largest distinct element from an array
+
+"""
+Given an array of positive integers arr[] of size n, 
+the task is to find second largest distinct element in the array.
+"""
+
