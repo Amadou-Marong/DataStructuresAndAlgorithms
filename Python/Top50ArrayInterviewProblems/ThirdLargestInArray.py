689b7ab53e30add7335254a9afc21001f5a6d3b0
Amadou-Marong <marongamadou6@gmail.com>
2025-01-05 10:48:50 +0000

    Third largest distinct element in an array

---
+++ Python/Top50ArrayInterviewProblems/ThirdLargestInArray.py
@@ -0,0 +1,43 @@
+# python program to find the third largest element in an array of distinct elements
+
+"""
+Naive Approach: The task is to first find the largest element, 
+followed by the second-largest element and then excluding them both 
+find the third-largest element. The basic idea is to iterate the array 
+twice and mark the maximum and second maximum element and then excluding 
+them both find the third maximum element, 
+i.e the maximum element excluding the maximum and second maximum.
+"""
+def getThirdLagest(arr):
+    
+    n = len(arr)
+    
+    largest = -1
+    secondLargest = -1
+    thirdLargest = -1
+    
+    # first iterate over the array to find the maximum. store the value alongside its index
+    
+    for i in range(n):
+        if arr[i] > largest:
+            largest = arr[i]
+        
+    # Now traverse the whole array finding the second max, excluding the maximum element.
+    for i in range(n):
+        if arr[i] > secondLargest and arr[i] != largest:
+            secondLargest = arr[i]
+    
+    # Finally traverse the array the third time and find the third largest element 
+    # i.e., excluding the maximum and second maximum.
+
+    for i in range(n):
+        if arr[i] > thirdLargest and arr[i] != secondLargest and arr[i] != largest:
+            thirdLargest = arr[i]
+    
+    return thirdLargest
+
+if __name__ == "__main__":
+    
+    arr = [12, 13, 1, 10, 34, 16]
+    
+    print(getThirdLagest(arr))
