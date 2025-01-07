42f5b3a31751555696c3f3c40d9922682fea1ef8
Amadou-Marong <marongamadou6@gmail.com>
2025-01-07 15:11:40 +0000

    Third largest distinct element in an array

--- Python/Top50ArrayInterviewProblems/ThirdLargestInArray.py
+++ Python/Top50ArrayInterviewProblems/ThirdLargestInArray.py
@@ -10,7 +10,8 @@ i.e the maximum element excluding the maximum and second maximum.
 
 Time O(n) space O(1)
 """
-def getThirdLagest(arr):
+"""
+def getThirdLargest(arr):
     
     n = len(arr)
     
@@ -43,3 +44,43 @@ if __name__ == "__main__":
     arr = [12, 13, 1, 10, 34, 16]
     
     print(getThirdLagest(arr))
+"""
+
+"""
+Efficient Approach: The problem deals with finding the third largest element in the array in 
+a single traversal. The problem can be cracked by taking help of a similar problem- finding the 
+second maximum element. So the idea is to traverse the array from start to end and to keep track 
+of the three largest elements up to that index (stored in variables). So after traversing the whole 
+array, the variables would have stored the indices (or value) of the three largest elements of the array.
+
+Time O(n) Space 0(1)
+"""
+def getThirdLargest(arr):
+    
+    n = len(arr)
+    
+    firstLargest = -1
+    secondLargest = -1
+    thirdLargest = -1
+    
+    for i in range(n):
+        if arr[i] > firstLargest:
+            firstLargest = arr[i]
+        
+        if arr[i] > firstLargest and arr[i] > secondLargest:
+            secondLargest = firstLargest
+            firstLargest = arr[i]
+        
+        if arr[i] > secondLargest and arr[i] > thirdLargest:
+            thirdLargest = secondLargest
+            secondLargest = arr[i]
+    
+    return thirdLargest
+
+if __name__ == '__main__':
+    arr = [12, 13, 1, 10, 34, 16] 
+
+    print(getThirdLargest(arr))
+
+
+
