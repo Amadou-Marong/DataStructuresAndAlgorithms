c842f1a73f8871cf0f4a014cc811b97723fa136f
Amadou-Marong <marongamadou6@gmail.com>
2025-01-11 16:32:58 +0000

    3 sum closest

---
+++ Python/Top50ArrayInterviewProblems/3SumClosestToTarget.py
@@ -0,0 +1,32 @@
+
+
+def closest3Sum(arr, target):
+    # code here
+    n = len(arr)
+    minDiff = float('inf')
+    closestSum = 0
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                currentSum = arr[i] + arr[j] + arr[k]
+                currentDiff = abs(target - currentSum)
+                
+                if currentDiff < minDiff:
+                    closestSum = currentSum
+                    minDiff = currentDiff
+                elif currentDiff == minDiff:
+                    closestSum = max(closestSum, currentSum)
+    
+    return closestSum
+    
+
+if __name__ == "__main__":
+    # arr = [-1, 2, 2, 4]
+    # target = 4
+    arr = [-1,2,1,-4]
+    target = 1
+    
+    print(closest3Sum(arr, target))
+    
+    
