a4718f1efce79477cac32805267826be54d9f077
Amadou-Marong <marongamadou6@gmail.com>
2025-01-19 09:40:16 +0000

    reverse array in groups of given size

--- Python/Top50ArrayInterviewProblems/ReverseArrayInGroups.py
+++ Python/Top50ArrayInterviewProblems/ReverseArrayInGroups.py
@@ -2,3 +2,25 @@
 
 def reverseArray(arr, k):
     n = len(arr)
+    
+    if k == 1:
+        return arr
+    
+    left = 0
+    right = n - 1
+    
+    while right > left:
+        arr[left], arr[right] = arr[right], arr[left]
+        
+        left += 1
+        right -= 1
+    
+    return arr
+
+# Driver code
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+k = 3
+
+print(reverseArray(arr, k)) 
+
+        
