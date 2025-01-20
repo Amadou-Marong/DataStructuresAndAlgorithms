a266b08fa804b8a9fe9a4e1004c6ff4cd71b7eec
Amadou-Marong <marongamadou6@gmail.com>
2025-01-20 14:46:40 +0000

    rotate the array elements to the left by d positions

---
+++ Python/Top50ArrayInterviewProblems/rotateArray.py
@@ -0,0 +1,16 @@
+def rotateArray(arr, d):
+    n = len(arr)
+    
+    for i in range(d):
+        
+        first = arr[0]
+    
+        for j in range(n-1):
+            arr[j] = arr[j+1]
+        arr[n-1] = first      
+        
+    return arr
+
+arr = [1, 2, 3, 4, 5, 6]
+d = 2
+print(rotateArray(arr, d))
