6be8879a1342bd0830c5b37f53917e043aa15c80
Amadou-Marong <marongamadou6@gmail.com>
2025-01-24 10:24:55 +0000

    rotate array by d positions

--- Python/Top50ArrayInterviewProblems/rotateArray.py
+++ Python/Top50ArrayInterviewProblems/rotateArray.py
@@ -5,6 +5,7 @@ In each iteration, shift the elements by one position to the left in a circular
 fashion (the first element becomes the last). Perform this operation d times to 
 rotate the elements to the left by d positions.
 """
+"""
 def rotateArray(arr, d):
     n = len(arr)
     
@@ -21,3 +22,27 @@ def rotateArray(arr, d):
 arr = [1, 2, 3, 4, 5, 6]
 d = 2
 print(rotateArray(arr, d))
+
+"""
+
+"""
+[Better Approach] Using Temporary Array – O(n) Time and O(n) Space
+This problem can be solved using the below idea:
+
+The idea is to use a temporary array of size n, where n is the length of the 
+original array. If we left rotate the array by d positions, the last n – d elements will 
+be at the front and the first d elements will be at the end.
+"""
+
+def rotateArray(arr, d):
+    n = len(arr)
+    temp = [0] * n
+    
+    for i in range(n-d):
+        print(arr[i])
+
+
+arr = [1, 2, 3, 4, 5, 6]
+d = 2
+
+rotateArray(arr, 2)
