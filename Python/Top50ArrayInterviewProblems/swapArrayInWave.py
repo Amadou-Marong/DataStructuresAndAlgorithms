455989658cc2e1178f76624f08521c746c467b8f
Amadou-Marong <marongamadou6@gmail.com>
2025-01-31 15:28:47 +0000

    swap array in wave form

--- Python/Top50ArrayInterviewProblems/swapArrayInWave.py
+++ Python/Top50ArrayInterviewProblems/swapArrayInWave.py
@@ -1,5 +1,14 @@
 # python program to swap array in wave form
+"""
+Naive Approach – Sorting – O(n Log n) time and O(1) Space
+A idea is to use sorting. First sort the input array, then swap all adjacent elements.
 
+Follow the steps mentioned below to implement the idea:
+
+Sort the array.
+Traverse the array from index 0 to n-1, and increase the value of the index by 2.
+While traversing the array swap arr[i] with arr[i+1].
+"""
 def swapArray(arr):
     n = len(arr)
     
@@ -13,3 +22,15 @@ def swapArray(arr):
 
 arr = [20, 10, 8, 6, 4, 2]        
 print(swapArray(arr))
+
+"""
+Expected Approach – O(n) Time and O(1) Space
+The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent odd elements, we don’t need to worry about oddly positioned elements. 
+
+
+Follow the steps mentioned below to implement the idea:
+
+Traverse all even positioned elements of the input array, and do the following. 
+If the current element is smaller than the previous odd element, swap the previous and current. 
+If the current element is smaller than the next odd element, swap next and current.
+"""
