74b9a01a3c94b41d24bfde2af0d1b7cce43906ae
Amadou-Marong <marongamadou6@gmail.com>
2025-01-31 15:36:43 +0000

    swap array in wave form

--- Python/Top50ArrayInterviewProblems/swapArrayInWave.py
+++ Python/Top50ArrayInterviewProblems/swapArrayInWave.py
@@ -9,23 +9,25 @@ Sort the array.
 Traverse the array from index 0 to n-1, and increase the value of the index by 2.
 While traversing the array swap arr[i] with arr[i+1].
 """
-def swapArray(arr):
-    n = len(arr)
+# def swapArray(arr):
+#     n = len(arr)
     
-    arr.sort()
+#     arr.sort()
     
-    for i in range(0, n-1, 2):
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+#     for i in range(0, n-1, 2):
+#         arr[i], arr[i+1] = arr[i+1], arr[i]
     
-    return arr
+#     return arr
 
 
-arr = [20, 10, 8, 6, 4, 2]        
-print(swapArray(arr))
+# arr = [20, 10, 8, 6, 4, 2]        
+# print(swapArray(arr))
 
 """
 Expected Approach – O(n) Time and O(1) Space
-The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent odd elements, we don’t need to worry about oddly positioned elements. 
+The idea is based on the fact that if we make sure that all even 
+positioned (at index 0, 2, 4, ..) elements are greater than their adjacent 
+odd elements, we don’t need to worry about oddly positioned elements. 
 
 
 Follow the steps mentioned below to implement the idea:
@@ -34,3 +36,17 @@ Traverse all even positioned elements of the input array, and do the following.
 If the current element is smaller than the previous odd element, swap the previous and current. 
 If the current element is smaller than the next odd element, swap next and current.
 """
+def swapArray(arr):
+    n = len(arr)
+    
+    #  traverse all even index of the array
+    for i in range(0, n-1, 2):
+        
+        # if array at current even index is less than the previous
+        if(i > 0 and arr[i] < arr[i-1]):
+            
+            # then we swap them
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+        
+            
+        
